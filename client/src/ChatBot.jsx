a740b9563edb658ee574638a5d1211e677f28602
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-04-12 20:41:30 +0530

    ChatBot.jsx

--- client/src/ChatBot.jsx
+++ client/src/ChatBot.jsx
@@ -22,8 +22,8 @@ const ChatBot = () => {
           <body>
             <df-messenger
               intent="WELCOME"
-              chat-title="EdTech-ChatBot"
-              agent-id="ae998d87-22ed-4a2b-9e20-67008786a608"
+              chat-title="NGO_EdTech-ChatBot"
+              agent-id="692d0042-a9c5-45ec-833f-a547a80d1ed4"
               language-code="en"
             ></df-messenger>
           </body>
