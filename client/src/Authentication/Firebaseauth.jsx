e97aa09a2cb6332579512731554a907ec57f7f59
Sagar Raj <121846695+Sagarraj2001@users.noreply.github.com>
2025-04-16 17:48:12 +0530

    Update Firebaseauth.jsx

--- client/src/Authentication/Firebaseauth.jsx
+++ client/src/Authentication/Firebaseauth.jsx
@@ -8,13 +8,13 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHYiLeW0GdsDP27g-qMnocjYoJnzeOCLY",
-  authDomain: "christian-ngo.firebaseapp.com",
-  projectId: "christian-ngo",
-  storageBucket: "christian-ngo.firebasestorage.app",
-  messagingSenderId: "164078942576",
-  appId: "1:164078942576:web:4de7f100e6336bd584b7b5",
-  measurementId: "G-RDD31HCELS"
+ apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
